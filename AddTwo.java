bda4f4163fe5183aacf3c843b416803fc63d2e9f
Itamar-Y <itamar.yaron@post.runi.ac.il>
2024-01-25 10:09:40 +0200

    AddTwo_Final

--- AddTwo.java
+++ AddTwo.java
@@ -2,10 +2,17 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
+	/**
+	 */
 	public static void main(String[] args) {
 		// Put your code here	
+		int number1 = Integer.parseInt(args[0]);
+		int number2 = Integer.parseInt(args[1]);
+		int Result = number1 + number2;
 
 
-		
+		System.out.println("The sum of " + number1 + " and " + number2 + " is " + Result);
 	}
 }
+
+
