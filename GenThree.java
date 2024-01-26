52738bed3b8a9dd9160e88a8f485afc513cd43b9
Itamar-Y <itamar.yaron@post.runi.ac.il>
2024-01-26 19:19:46 +0200

    Third Commit - HW01

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,17 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int var1 = Integer.parseInt(args[0]);
+		int var2 = Integer.parseInt(args[1]);
+
+		int gen1 = (var1 + ((int) (Math.random() * (var2-var1))));
+		int gen2 = (var1 + ((int) (Math.random() * (var2-var1))));
+		int gen3 = (var1 + ((int) (Math.random() * (var2-var1))));
+
+
+		System.out.println(gen1);
+		System.out.println(gen2);
+		System.out.println(gen3);
+		System.out.println("the minimal generated number is " + Math.min((Math.min(gen1, gen2)), gen3));
 	}
 }
