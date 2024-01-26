e6b361a7cee39ccf25ad1a15b1a990d018b4c093
Itamar-Y <itamar.yaron@post.runi.ac.il>
2024-01-26 23:29:10 +0200

    Commit seven - HW01

--- GenThree.java
+++ GenThree.java
@@ -4,18 +4,23 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
+
+		// arguments for range
 		int var1 = Integer.parseInt(args[0]);
 		int var2 = Integer.parseInt(args[1]);
 
-		int gen1 = (var1 + ((int) (Math.random() * (var2-var1))));
-		int gen2 = (var1 + ((int) (Math.random() * (var2-var1))));
-		int gen3 = (var1 + ((int) (Math.random() * (var2-var1))));
+		//random number generator withing range
+		int gen1 = var1 + (int) (Math.random() * (var2-var1));
+		int gen2 = var1 + (int) (Math.random() * (var2-var1));
+		int gen3 = var1 + (int) (Math.random() * (var2-var1));
 
-		int Min = (int) Math.min((Math.min(gen1, gen2)), gen3);
-
+		// print of the gen numbers
 		System.out.println(gen1);
 		System.out.println(gen2);
 		System.out.println(gen3);
+
+		int Min = (int) Math.min((Math.min(gen1, gen2)), gen3);
+
 		System.out.println("The minimal generated number was " + Min);
 	}
 }
